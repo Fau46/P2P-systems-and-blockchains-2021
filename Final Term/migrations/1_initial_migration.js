12e2d79d727a71953c88254674fe1c65d8b964ab
Fausto <frasca.fausto@gmail.com>
2021-05-22 22:36:24 +0200

    Added gas consumed in the tests

--- Final Term/migrations/1_initial_migration.js
+++ Final Term/migrations/1_initial_migration.js
@@ -5,8 +5,22 @@ const Mayor = artifacts.require("Mayor");
 module.exports = async (deployer,newtwork,accounts) => {
   await deployer.deploy(Migrations);
 
-  const candidate = accounts[1];
-  const escrow = accounts[9];
-  console.log(candidate)
-  await deployer.deploy(Mayor, candidate, escrow, 2);
+  // const candidate = accounts[1];
+  // const escrow = accounts[9];
+
+  // const voter_1 = accounts[2];
+  // const voter_2 = accounts[3];
+
+  // const instance = await deployer.deploy(Mayor, candidate, escrow, 2);
+  // const ce1 = await instance.compute_envelope(1,false, web3.utils.toWei("50", "ether"), {from: voter_1});
+  // await instance.cast_envelope(ce1, {from: voter_1});
+  // console.log("voter 1 voted");
+  // const ce2 = await instance.compute_envelope(1,true, web3.utils.toWei("1", "ether"), {from: voter_2});
+  // await instance.cast_envelope(ce2,{from: voter_2});
+  // console.log("voter 2 voted");
+  // await instance.open_envelope(1,false, {from: voter_1, value: web3.utils.toWei("50", "ether")})
+  // await instance.open_envelope(1,true, {from: voter_2, value: web3.utils.toWei("1", "ether")})
+  // console.log("envelope opened");
+  // await instance.mayor_or_sayonara();
+  // console.log("mayor elected")
 };
--- Final Term/test/my_test.js
+++ Final Term/test/my_test.js
@@ -1,5 +1,6 @@
 const Mayor = artifacts.require("Mayor");
-var web3 = require("web3")
+const web3 = require("web3")
+
 
 contract("Testing Mayor", accounts => {
   const deployer = accounts[0];
@@ -11,19 +12,42 @@ contract("Testing Mayor", accounts => {
 
   // const eth = web3.utils.toBN(1000000000000000000);
   const eth = 1;
+  var instance;
+  var ce1;
+  var ce2;
 
-  it("Should test the voter phase", async function() {
-    const instance = await Mayor.new(candidate, escrow, 2, {from: deployer});
-    const ce1 = await instance.compute_envelope(1,true, eth, {from: voter_1});
-    await instance.cast_envelope(ce1, {from: voter_1});
-    const ce2 = await instance.compute_envelope(1,false, eth * 10, {from: voter_2});
+  it("Should test constructor", async function() {
+    instance = await Mayor.new(candidate, escrow, 2, {from: deployer});
+    // instance = await Mayor.new.estimateGas(candidate, escrow, 2, {from: deployer});
+    // console.log("Estimate gas " + instance);
+  });
+  
+
+  it("Should test compute_envelope", async function(){
+    // ce1 = await instance.compute_envelope.estimateGas(1,true, web3.utils.toWei("50", "ether"), {from: voter_1});
+    ce1 = await instance.compute_envelope(1,true, web3.utils.toWei("50", "ether"), {from: voter_1});
+    ce2 = await instance.compute_envelope(1,false, web3.utils.toWei("1", "ether"), {from: voter_2});
+    // console.log(ce1);
+  })
+
+  it("Should test cast_envelope", async function(){
+    // const result = await instance.cast_envelope.estimateGas(ce1, {from: voter_1});
     await instance.cast_envelope(ce1, {from: voter_1});
     await instance.cast_envelope(ce2,{from: voter_2});
-    await instance.open_envelope(1,true, {from: voter_1, value: eth})
-    await instance.open_envelope(1,false, {from: voter_2, value: eth * 10})
-    const result = await instance.mayor_or_sayonara({from: deployer});
+    // console.log(result);
+  })
 
+  it("Should test open_envelope", async function(){
+    // const result = await instance.open_envelope.estimateGas(1,true, {from: voter_1, value: web3.utils.toWei("50", "ether")})
+    await instance.open_envelope(1,true, {from: voter_1, value: web3.utils.toWei("50", "ether")})
+    await instance.open_envelope(1,false, {from: voter_2, value: web3.utils.toWei("1", "ether")})
+    // console.log(result)
+  })
+
+  it("Should test mayor_or_sayonara", async function(){
+    const result = await instance.mayor_or_sayonara.estimateGas({from: deployer});
+    // const result = await instance.mayor_or_sayonara({from: deployer});
     console.log(result)
-    assert(instance.yaySoul, 0, "errore");
-  }); 
+  })
+  
  });
